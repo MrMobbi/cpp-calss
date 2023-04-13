5c90a46af616b282281536bb60f54cafff36389b
MrMobbi <julliatmathias@gmail.com>
2023-04-13 13:23:49 +0200

    ex00 added

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 
-
-int	main()
+int	main(int ac, char **av)
 {
-
+	if (ac != 1)
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+				av[i][j] = std::toupper(av[i][j]);
+			std::cout << av[i] << std::endl;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
 }
