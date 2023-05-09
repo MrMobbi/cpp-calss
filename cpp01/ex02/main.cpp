36d21e24f0e700537855abb4638abda06d6725e2
MrMobbi <julliatmathias@gmail.com>
2023-05-09 19:17:00 +0200

    ex03 modified

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -8,13 +8,13 @@ int	main(void)
 	std::string	&ref = str;
 
 	std::cout << "address of str : " << &str << std::endl;
-	std::cout << "address of ptr : " << &ptr << std::endl;
+	std::cout << "address of ptr : " << ptr << std::endl;
 	std::cout << "address of ref : " << &ref << std::endl;
 
 	std::cout << std::endl;
 
 	std::cout << "value of str : " << str << std::endl;
-	std::cout << "value of ptr : " << ptr << std::endl;
+	std::cout << "value of ptr : " << *ptr << std::endl;
 	std::cout << "value of ref : " << ref << std::endl;
 	return (0);
 }
