d86e5d403d69af023ecddd31a2bdb54e20d3d340
MrMobbi <julliatmathias@gmail.com>
2023-05-09 19:13:54 +0200

    ex03 added

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string	*ptr = &str;
+	std::string	&ref = str;
+
+	std::cout << "address of str : " << &str << std::endl;
+	std::cout << "address of ptr : " << &ptr << std::endl;
+	std::cout << "address of ref : " << &ref << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "value of str : " << str << std::endl;
+	std::cout << "value of ptr : " << ptr << std::endl;
+	std::cout << "value of ref : " << ref << std::endl;
+	return (0);
+}
