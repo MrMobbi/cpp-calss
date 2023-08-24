e22c298dcaf1aa5a0c798a91ed143967e4f85d21
MrMobbi <julliatmathias06@gmail.com>
2023-08-24 10:38:52 +0200

    WIP

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,12 +2,74 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-template<typename t_type>
+# include <iostream>
+
+template<typename t_type = int> 
 class Array {
 
 	public :
-		Array(void);
-		~Array(void);
+
+//	### Constructor
+		Array(void) : _size(0)
+		{
+			this->_element = NULL;
+		};
+		Array(unsigned int n) : _size(n)
+		{
+			this->_element = new t_type[n]();
+		};
+		Array(const Array &t) : _size(t._size)
+		{
+			this->_element = new t_type[t._size]();
+			for (unsigned int i = 0; i < t._size; i++)
+				this->_element[i] = t._element[i];
+		}
+
+//	### Overload operator'='
+		Array &operator = (const Array &t)
+		{
+			if (this->_element != NULL)
+				delete[] (this->_element);
+			this->_size = t._size;
+			this->_element = new t_type[t._size]();
+			for (unsigned int i = 0; i < t._size; i++)
+				this->_element[i] = t._element[i];
+			return (*this);
+		};
+
+//	### Destructor
+		~Array(void) 
+		{
+			delete[] (this->_element);
+		};
+
+//	### Exception
+		class IndexException : public std::exception
+		{
+			public :
+				virtual const char *what(void) const throw()
+				{
+					return ("Index is not Valid");
+				}
+		};
+
+//	### Overload operator '[]'
+		t_type &operator [] (const int i) const
+		{
+			if (i < 0 || i > static_cast<int>(this->_size) - 1)
+				throw(IndexException());
+			return (this->_element[i]);
+		};
+
+//	### Member Function
+		unsigned int	size(void) const
+		{	
+			return (this->_size); 
+		};
+
+	private :
+		t_type				*_element;
+		unsigned int	_size;
 };
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,3 +1,52 @@
 
 #include <iostream>
 #include "Array.hpp"
+
+#define	VALUE 5
+
+int	main(void)
+{
+	Array	<float>test(VALUE);
+
+	for (int i = 0; i < VALUE; i++)
+		test[i] = i;
+
+	Array	<float>truc(test);
+	Array	<float>truc2;
+
+	test[1] = 1000;
+	truc2 = test;
+	test[2] = 2000;
+
+	std::cout << "Printing test" << std::endl;
+	for (int i = 0; i < VALUE; i++)
+		std::cout << test[i] << std::endl;
+
+	std::cout << "Printing truc" << std::endl;
+	for (int i = 0; i < VALUE; i++)
+		std::cout << truc[i] << std::endl;;
+
+	std::cout << "Printing truc2" << std::endl;
+	for (int i = 0; i < VALUE; i++)
+		std::cout << truc2[i] << std::endl;
+
+	try
+	{
+		std::cout << test[-1] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << test[VALUE] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
+}
