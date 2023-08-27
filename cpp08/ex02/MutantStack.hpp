aaddee0b534b3cdc4396d5104554c64da3c4b371
MrMobbi <julliatmathias06@gmail.com>
2023-08-27 22:37:47 +0200

    WIP

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <stack>
+#include <iterator>
 #include <algorithm>
 
 template<typename t_type>
@@ -11,14 +12,29 @@ class	MutantStack : public std::stack<t_type>
 {
 	public :
 		MutantStack(void) {};
-		MutantStack(MutantStack &t) {
+		MutantStack(MutantStack &t) : std::stack<t_type>(t)
+		{
 			*this = t;
 		}
-		MutantStack &operator = (MutantStack &t) {
+		MutantStack &operator = (MutantStack &t) 
+		{
 			std::stack<t_type>::operator=(t);
 			return (*this);
 		}
 		~MutantStack(void) {};
+
+		typedef typename std::stack<t_type>::container_type ::iterator	iterator;
+		typedef typename std::stack<t_type>::container_type ::const_iterator	const_iterator;
+		
+		iterator	begin(void)
+		{
+			return (this->c.begin());
+		}
+
+		iterator	end(void)
+		{
+			return (this->c.end());
+		}
 };
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,12 +6,34 @@
 
 int	main(void)
 {
-	MutantStack<int>	test;
-	MutantStack<int>	truc(test);
-	MutantStack<int>	truc2;
+	MutantStack<int>	mutant;
+	mutant.push(1);
+	MutantStack<int>	mutant2(mutant);
+	MutantStack<int>	mutant3;
 
-	truc2 = test;
-	test.push(5);
-	std::cout << test.top() << std::endl;
+	mutant.push(5);
+	mutant.push(34);
+	mutant.push(23);
+	mutant.push(63);
+	mutant.push(29);
+
+	mutant3 = mutant;
+	std::cout << "Testing Mutant3 :" << std::endl;
+	std::cout << mutant3.top() << std::endl;
+	std::cout << "Testing Mutant2 :" << std::endl;
+	std::cout << mutant2.top() << std::endl;
+	std::cout << "Testing Mutant :" << std::endl;
+	std::cout << mutant.top() << std::endl;
+
+	MutantStack<int>::iterator	it = mutant.begin();
+	MutantStack<int>::iterator	ite = mutant.end();
+	std::cout << "Going into the MutantStack by iterator :" << std::endl;
+	for (;it != ite; it++)
+		std::cout << *it << std::endl;
+	it--;
+	std::cout << "Going revrese :" << std::endl;
+	for (;it != mutant.begin(); it--)
+		std::cout << *it << std::endl;
+	std::cout << *it << std::endl;
 	return (0);
 }
