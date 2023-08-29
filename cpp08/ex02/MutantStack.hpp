2cd39f76b95845ad1ac326092ac3f3ff619bfc5a
Mathias Julliat <mjulliat@c3r4s3.42lausanne.ch>
2023-08-29 15:56:56 +0200

    WIP

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -25,8 +25,16 @@ class	MutantStack : public std::stack<t_type>
 
 		typedef typename std::stack<t_type>::container_type ::iterator	iterator;
 		typedef typename std::stack<t_type>::container_type ::const_iterator	const_iterator;
-		
+
+		typedef typename std::stack<t_type>::container_type ::reverse_iterator	reverse_iterator;
+		typedef typename std::stack<t_type>::container_type ::const_reverse_iterator	const_reverse_iterator;
+
 		iterator	begin(void)
+		{
+			return (this->c.begin());
+		}
+
+		const_iterator	begin(void) const
 		{
 			return (this->c.begin());
 		}
@@ -34,6 +42,31 @@ class	MutantStack : public std::stack<t_type>
 		iterator	end(void)
 		{
 			return (this->c.end());
+		}
+
+		const_iterator	end(void) const
+		{
+			return (this->c.end());
+		}
+
+		reverse_iterator	rbegin(void)
+		{
+			return (this->c.rbegin());
+		}
+
+		const_reverse_iterator	rbegin(void) const
+		{
+			return (this->c.rbegin());
+		}
+
+		reverse_iterator	rend(void)
+		{
+			return (this->c.rend());
+		}
+
+		const_reverse_iterator	rend(void) const
+		{
+			return (this->c.rend());
 		}
 };
 
